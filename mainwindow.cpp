749061ad7e3ae1c42a5cf65d4b19919add18d0b7
SeppNel <35899928+SeppNel@users.noreply.github.com>
2021-11-09 19:23:36 +0100

    Changed ReadMultiplePointer to the non safe version

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <windows.h>
 #include <QKeyEvent>
+//#include <QDebug>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -41,21 +43,14 @@ void MainWindow::updateUI(QString text, QString style)
 }
 
 
-// Function to read pointers (https://github.com/Ezekial711/MonsterHunterWorldModding/wiki/Making-a-persistent-loop-plugin)
-
+// Function to read pointers, using the non safe version because the safe version enters an infinite loop in some cases (https://github.com/Ezekial711/MonsterHunterWorldModding/wiki/Making-a-persistent-loop-plugin)
 template<class T>
-T* ReadMultiLevelPointerSafe(HANDLE process_handle, void* base_address, const std::vector<uintptr_t>& offsets)
+T* ReadMultiLevelPointer(HANDLE process_handle, void* base_address, const std::vector<uintptr_t>& offsets)
 {
     uintptr_t addr = (uintptr_t)base_address;
     for (const auto& offset : offsets)
     {
-        uintptr_t tmp = (uintptr_t)nullptr;
-        do {
-            Sleep(20);
-            ReadProcessMemory(process_handle, (void*)addr, &tmp, sizeof(tmp), NULL);
-        } while (tmp == (uintptr_t)nullptr);
-
-        addr = tmp;
+        ReadProcessMemory(process_handle, (void*)addr, &addr, sizeof(addr), NULL);
         addr += offset;
     }
 
@@ -92,14 +87,14 @@ void MainWindow::get_damage()
     int damageval = 0;
     while (true)
     {
-        float* damage = ReadMultiLevelPointerSafe<float>(hProcess, (void*)0x14BF036A8, { 0x4C8, 0x18 });
+        float* damage = ReadMultiLevelPointer<float>(hProcess, (void*)0x14BF036A8, { 0x4C8, 0x18 });
         ReadProcessMemory(hProcess, (void*)damage, &damageval, sizeof(damageval), 0);
 
         QString text = QString("Daño: %1").arg(damageval);
         emit uiSignal(text, style);
 
 
-        Sleep(100);
+        Sleep(350);
     }
 
 }
@@ -115,29 +110,32 @@ void MainWindow::get_hp()
     float monsterHPval = 0;
     char buffer[8];
     float maxHP = 0;
-
+    
     while (true)
     {
-
-        float* monsterHP = ReadMultiLevelPointerSafe<float>(hProcess, (void*)0x14BEF7738, { 0x80, 0x40, 0x290, 0x48, 0x40, 0x40, 0x10 });
+        float* monsterHP = ReadMultiLevelPointer<float>(hProcess, (void*)0x14BEF7738, { 0x80, 0x40, 0x290, 0x48, 0x40, 0x40, 0x10 });
         //ReadProcessMemory(hProcess, (void*)monsterHP, &monsterHPval, sizeof(monsterHPval), 0);
-        ReadProcessMemory(hProcess, (void*)monsterHP, &buffer, 8, 0);
+        bool read_success = ReadProcessMemory(hProcess, (void*)monsterHP, &buffer, 8, 0);
 
-        memcpy(&monsterHPval, &buffer, 4);
-        memcpy(&maxHP, &buffer[4], 4);
+        if (read_success) {
+            memcpy(&monsterHPval, &buffer, 4);
+            memcpy(&maxHP, &buffer[4], 4);
 
-        //qDebug() << monsterHPval << " - " << maxHP;
-        
-        QString text = QString("%1 / %2").arg(int(monsterHPval + 0.5)).arg(int(maxHP + 0.5));
+            //qDebug() << monsterHPval << " - " << maxHP;
 
-        float percent = monsterHPval / maxHP;
-        percent = roundf(percent * 100) / 100;
-        QString style = "background-color: qlineargradient(spread:pad, x1:" + QString::number(percent - 0.01) + ", y1:1, x2:" + QString::number(percent) + ", y2:1, stop:0 red, stop:1 rgba(140, 140, 140, 0.6));";
+            QString text = QString("%1 / %2").arg(int(monsterHPval + 0.5)).arg(int(maxHP + 0.5));
 
-        emit uiSignal(text, style);
+            float percent = monsterHPval / maxHP;
+            percent = roundf(percent * 100) / 100;
+            QString style = "background-color: qlineargradient(spread:pad, x1:" + QString::number(percent - 0.01) + ", y1:1, x2:" + QString::number(percent) + ", y2:1, stop:0 red, stop:1 rgba(140, 140, 140, 0.6));";
 
+            emit uiSignal(text, style);
+        }
+        else {
+            emit uiSignal("Esperando", style);
+        }
 
-        Sleep(100);
+        Sleep(250);
     }
 
 }
@@ -150,10 +148,11 @@ void fly_mode()
     float val = 0;
     while (true)
     {
-        //Read monster HP at start to block execution if not in a hunt, because otherwise this function can read a pointer mid change in the menu and softlock itself, he.
-        ReadMultiLevelPointerSafe<float>(hProcess, (void*)0x14BEF7738, { 0x80, 0x40, 0x290, 0x48, 0x40, 0x40, 0x10 });
+        //Read monster HP at start to block execution if not in a hunt, because otherwise this function can read a pointer mid change in the menu and softlock itself, heh.
+        //Not really needed if using the non safe version.
+        //ReadMultiLevelPointerSafe<float>(hProcess, (void*)0x14BEF7738, { 0x80, 0x40, 0x290, 0x48, 0x40, 0x40, 0x10 });
 
-        float* wirebug = ReadMultiLevelPointerSafe<float>(hProcess, (void*)0x14BEF63E8, { 0x60, 0x20, 0x3B0, 0x28, 0x10 });
+        float* wirebug = ReadMultiLevelPointer<float>(hProcess, (void*)0x14BEF63E8, { 0x60, 0x20, 0x3B0, 0x28, 0x10 });
         ReadProcessMemory(hProcess, (void*)wirebug, &val, sizeof(val), 0);
 
         if(val > 0){
@@ -161,7 +160,7 @@ void fly_mode()
             int returnval = WriteProcessMemory(hProcess, (void*)wirebug, &val, sizeof(val), 0);
         }
 
-        Sleep(16);
+        Sleep(150);
     }
 
 }
